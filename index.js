ae6f043d5a5fa8a0ea92fe403f2e31dde26cb786
Unstableknight <mastertechcraft@gmail.com>
2018-09-17 19:13:35 -0400

    fixed embed Commands

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("Milkshake-boi/config.json");
+const config = require("./config.json");
 var bot = new Discord.Client();
 bot.on('ready',() => {
   console.log('Im online!');
@@ -7,13 +7,11 @@ bot.on('ready',() => {
 
 bot.on("message", function(message) {
   if (message.author.equals(bot.user)) return;
-
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(config.prefix)) return;
   
-  var args = message.content.substring(prefix.length).split(" ");
-  let embed = new Discord.RichEmbed()
+  var args = message.content.substring(config.prefix.length).split(" ");
   switch (args[0].toLowerCase()) {
-    case "ip myth":
+    case "milkip":
         message.channel.send("MILKSHAKELAND134.MC-DNS.NET");
         break;
     case "help":
@@ -22,24 +20,24 @@ bot.on("message", function(message) {
         .setDescription("This is where you can find all the commands!")
         .addField("Prefix", "*")
         .setColor("#9B59B6")
-        .addField("ip myth", "Gives you Mythical's MC server IP")
+        .addField("milkip", "Gives you Mythical's MC server IP")
         .addField("test2")
         .addField("test3");
 
         message.channel.send(embed);
         break;
     case "servers":
-    new Discord.RichEmbed()
+     let embed3 = new Discord.RichEmbed()
             .setTitle("Server List")
             .setDescription("This is The Server List!")
             .setColor("#dd6a18")
             .addField("Nates MC Server", "Nateb0b.aternos.me")
             .addField("Mythicals MC server", "MILKSHAKELAND134.MC-DNS.NET");
 
-        message.channel.send(embed);
+        message.channel.send(embed3);
       break;
     case "info":
-    new Discord.RichEmbed()
+    let embed2 = new Discord.RichEmbed()
         .setTitle("Info About Bot!")
         .setColor("#32ff2b")
         .addField("Author", "MilkshakeAddict")
@@ -47,7 +45,7 @@ bot.on("message", function(message) {
         .addField("Author's Discord", "https://discord.gg/PzTZhK")
         .addField("Author's Twitch", "https://www.twitch.tv/milkshake_addict");
 
-    message.channel.send(embed);
+    message.channel.send(embed2);
     break;
   }
 

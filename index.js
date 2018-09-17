fbca0f3007b5b352fe01e04f0a0f59fe611a1691
Unstableknight <mastertechcraft@gmail.com>
2018-09-17 16:36:47 -0400

    big change

--- index.js
+++ index.js
@@ -6,22 +6,17 @@ bot.on('ready',() => {
 });
 
 bot.on("message", function(message) {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  let args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.content.startsWith(config.prefix + "ip myth")) {
-    message.channel.send("MILKSHAKELAND134.MC-DNS.NET");
-  } else
+  if (message.author.equals(bot.user)) return;
 
-  if (message.content.startsWith(config.prefix + 'test')) {
-    message.channel.send('bar');
-  } else
-
-  if (message.content.startsWith(config.prefix + 'setgame')) {
-  if(message.author.id !== config.ownerID) return;
-    bot.user.setActivity(argresult);
-  } else
-  if (message.content.startsWith(config.prefix + 'help')) {
+  if (!message.content.startsWith(prefix)) return;
+  
+  var args = message.content.substring(prefix.length).split(" ");
+  let embed = new Discord.RichEmbed()
+  switch (args[0].toLowerCase()) {
+    case "ip myth":
+        message.channel.send("MILKSHAKELAND134.MC-DNS.NET");
+        break;
+    case "help":
     let embed = new Discord.RichEmbed()
         .setTitle("Help Menu")
         .setDescription("This is where you can find all the commands!")
@@ -31,10 +26,20 @@ bot.on("message", function(message) {
         .addField("test2")
         .addField("test3");
 
-    message.channel.send(embed);
-  } else
-  if (message.content.startsWith(config.prefix + 'info')) {
-    let embed = new Discord.RichEmbed()
+        message.channel.send(embed);
+        break;
+    case "servers":
+    new Discord.RichEmbed()
+            .setTitle("Server List")
+            .setDescription("This is The Server List!")
+            .setColor("#dd6a18")
+            .addField("Nates MC Server", "Nateb0b.aternos.me")
+            .addField("Mythicals MC server", "MILKSHAKELAND134.MC-DNS.NET");
+
+        message.channel.send(embed);
+      break;
+    case "info":
+    new Discord.RichEmbed()
         .setTitle("Info About Bot!")
         .setColor("#32ff2b")
         .addField("Author", "MilkshakeAddict")
@@ -43,16 +48,13 @@ bot.on("message", function(message) {
         .addField("Author's Twitch", "https://www.twitch.tv/milkshake_addict");
 
     message.channel.send(embed);
+    break;
   }
-  if (message.content.startsWith(config.prefix + 'servers')) {
-    let embed = new Discord.RichEmbed()
-        .setTitle("Server List")
-        .setDescription("This is The Server List!")
-        .setColor("#dd6a18")
-        .addField("Nates MC Server", "Nateb0b.aternos.me")
-        .addField("Mythicals MC server", "MILKSHAKELAND134.MC-DNS.NET");
 
-    message.channel.send(embed);
+
+  if (message.content.startsWith(config.prefix + 'setgame')) {
+  if(message.author.id !== config.ownerID) return;
+    bot.user.setActivity(argresult);
   }
 });
 
